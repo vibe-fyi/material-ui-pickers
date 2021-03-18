dfd1449c96ee5f799e83b0c3de5285eaf7c672ce
nhale <nha@vibe.fyi>
2021-03-18 14:28:56 +1300

    Ceil the minute to the nearest 5 minutes - add custom dist

---
+++ lib/custom-dist/DatePicker/DatePickerToolbar.d.ts
@@ -0,0 +1,6 @@
+import * as React from 'react';
+import { ToolbarComponentProps } from '../Picker/Picker';
+export declare const useStyles: (
+  props?: any
+) => Record<'toolbar' | 'toolbarLandscape' | 'dateLandscape', string>;
+export declare const DatePickerToolbar: React.FC<ToolbarComponentProps>;
